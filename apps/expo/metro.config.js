4dd94a3c3af9b9a74890e3e0bceab43febbab64f
Raymond Z <raykyri@gmail.com>
2024-11-04 15:50:32 -0500

    config cleanup (sq)

--- apps/expo/metro.config.js
+++ apps/expo/metro.config.js
@@ -13,29 +13,28 @@ const config = getDefaultConfig(projectRoot)
 
 // 1. Watch all files within the monorepo
 config.watchFolders = [workspaceRoot]
+
 // 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(workspaceRoot, 'node_modules'),
 ]
-// 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
+
+// 3. Force Metro to resolve (sub)dependencies only from `nodeModulesPaths`
 config.resolver.disableHierarchicalLookup = true
 
-config.resolver.resolverMainFields = ['react-native', 'browser', 'main', 'lib', 'import']
-
+// 4. Read the `exports` field, instead of just `main`, in package.json
 config.resolver.unstable_enablePackageExports = true
 
 config.resolver.resolveRequest = (context, moduleName, platform) => {
-  console.log('========================================')
-  console.log('resolving', moduleName, platform, context.enablePackageExports)
   try {
     const result = context.resolveRequest(context, moduleName, platform)
-    console.log('----------------------------------------')
-    console.log(result)
-    console.log('========================================')
     return result
   } catch (e) {
+    console.log('========================================')
+    console.log('resolving', moduleName, platform, context.enablePackageExports)
     console.log('ERR', e)
+    console.log('========================================')
     throw e
   }
 }

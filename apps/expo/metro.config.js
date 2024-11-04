16c2185c28bdb671416e3ff1867d1258502e5651
Raymond Z <raykyri@gmail.com>
2024-11-04 17:21:03 -0500

    remove debug

--- apps/expo/metro.config.js
+++ apps/expo/metro.config.js
@@ -26,19 +26,6 @@ config.resolver.disableHierarchicalLookup = true
 // 4. Read the `exports` field, instead of just `main`, in package.json
 config.resolver.unstable_enablePackageExports = true
 
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-  try {
-    const result = context.resolveRequest(context, moduleName, platform)
-    return result
-  } catch (e) {
-    console.log('========================================')
-    console.log('resolving', moduleName, platform, context.enablePackageExports)
-    console.log('ERR', e)
-    console.log('========================================')
-    throw e
-  }
-}
-
 config.transformer = { ...config.transformer, unstable_allowRequireContext: true }
 config.transformer.minifierPath = require.resolve('metro-minify-terser')
 

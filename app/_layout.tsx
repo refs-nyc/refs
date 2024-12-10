bfe7542c89958d153e262504633fc3659a95861d
Raymond Z <raykyri@gmail.com>
2024-12-09 20:34:33 -0500

    feat: add magic login, store as session signer

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,25 @@
-import { install } from "react-native-quick-crypto"
+// polyfills
+import { install } from 'react-native-quick-crypto'
 install()
 
-import { Stack } from "expo-router"
+import { polyfill as polyfillEncoding } from 'react-native-polyfill-globals/src/encoding'
+polyfillEncoding()
+
+import 'event-target-polyfill'
+import './custom-event-polyfill'
+
+// dependencies
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { Stack } from 'expo-router'
+
+import { magic } from './shared'
 
 export default function RootLayout() {
-  return <Stack />
+  return (
+    <SafeAreaProvider>
+      {/* Render the Magic iframe! */}
+      <magic.Relayer />
+      <Stack />
+    </SafeAreaProvider>
+  )
 }

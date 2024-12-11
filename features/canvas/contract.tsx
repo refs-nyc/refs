2ae56fdc3d27bbc94a54c85506646990a05fc2e9
Raymond Z <raykyri@gmail.com>
2024-12-11 15:12:46 -0500

    chore: refactor contract in preparation for server

--- features/canvas/contract.tsx
+++ features/canvas/contract.tsx
@@ -2,7 +2,7 @@ import 'event-target-polyfill'
 import 'fast-text-encoding'
 
 import { models } from './models'
-import { Canvas } from '@canvas-js/core'
+import { Canvas, Contract } from '@canvas-js/core'
 import { createContext, useContext, useEffect, useState } from 'react'
 
 export const CanvasContext = createContext(null)
@@ -11,82 +11,78 @@ export function useCanvasContext() {
   return useContext(CanvasContext)
 }
 
+export const contract = {
+  models,
+  actions: {
+    // Profiles
+    createProfile(db, { firstName, lastName, userName, items = [], image = null }) {
+      db.create('profiles', {
+        did: this.did,
+        firstName,
+        lastName,
+        userName,
+        items,
+        image,
+        location: null,
+        geolocation: null,
+      })
+
+      return { did: this.did, userName }
+    },
+    // ***
+    // Refs
+    //
+    //
+    //
+    createRef(db, { title, firstReferral = null, image = null, location = null, referrals = [] }) {
+      db.create('refs', {
+        id: this.id,
+        title,
+        image,
+        createdAt: new Date().getTime(),
+        deletedAt: null,
+        firstReferral,
+        location,
+        referrals,
+      })
+
+      return this.id
+    },
+    //
+    // ***
+    // Items
+    //
+    //
+    //
+    createItem(db, { ref, text = null, image = null, location = null, url = null, children = [] }) {
+      db.create('items', {
+        id: this.id,
+        ref,
+        createdAt: new Date().getTime(),
+        deletedAt: null,
+        text,
+        image,
+        location,
+        url,
+        children,
+      })
+
+      return this.id
+    },
+    updateItem(db, id, ref, text, image, location, url, children) {
+      db.update('items', { id, ref, text, image, location, url, children })
+    },
+    deleteItem(db, id) {
+      db.update('items', { id, deletedAt: new Date().getTime() })
+    },
+    // Chats
+  },
+} satisfies Contract<typeof models>
+
 const init = async () => {
   const app = await Canvas.initialize({
     //
-    contract: {
-      models,
-      actions: {
-        // Profiles
-        createProfile(db, { firstName, lastName, userName, items = [], image = null }) {
-          db.create('profiles', {
-            did: this.did,
-            firstName,
-            lastName,
-            userName,
-            items,
-            image,
-            location: null,
-            geolocation: null,
-          })
-
-          return { did: this.did, userName }
-        },
-        // ***
-        // Refs
-        //
-        //
-        //
-        createRef(
-          db,
-          { title, firstReferral = null, image = null, location = null, referrals = [] }
-        ) {
-          db.create('refs', {
-            id: this.id,
-            title,
-            image,
-            createdAt: new Date().getTime(),
-            deletedAt: null,
-            firstReferral,
-            location,
-            referrals,
-          })
-
-          return this.id
-        },
-        //
-        // ***
-        // Items
-        //
-        //
-        //
-        createItem(
-          db,
-          { ref, text = null, image = null, location = null, url = null, children = [] }
-        ) {
-          db.create('items', {
-            id: this.id,
-            ref,
-            createdAt: new Date().getTime(),
-            deletedAt: null,
-            text,
-            image,
-            location,
-            url,
-            children,
-          })
-
-          return this.id
-        },
-        updateItem(db, id, ref, text, image, location, url, children) {
-          db.update('items', { id, ref, text, image, location, url, children })
-        },
-        deleteItem(db, id) {
-          db.update('items', { id, deletedAt: new Date().getTime() })
-        },
-        // Chats
-      },
-    },
+    contract,
     topic: 'refsv2.canvas.xyz',
   })
 

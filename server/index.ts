df66a93a7b1eb514f9dce69d499348688ee74b77
Raymond Z <raykyri@gmail.com>
2024-12-12 11:40:49 -0500

    feat: add event listener for new actions

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,8 @@
+import type { CanvasEvents } from '@canvas-js/core'
+import type { GossipLogEvents } from '@canvas-js/gossiplog'
+import type { Action, Message, Session } from '@canvas-js/interfaces'
 import { contract, init } from '../features/canvas/contract.ts'
+import { TypedEventEmitter } from '@libp2p/interface'
 
 const DATABASE_URL = process.env.DATABASE_URL
 
@@ -10,7 +14,7 @@ const LIBP2P_ANNOUNCE_HOST = process.env.LIBP2P_ANNOUNCE_HOST || 'refs.canvas.xy
 const LIBP2P_ANNOUNCE_PORT = parseInt(process.env.LIBP2P_ANNOUNCE_PORT || '80', 10)
 
 init().then((app) => {
-  app.listen(PORT, HTTP_ADDR).then(() => {
+  app.listen(PORT).then(() => {
     console.log('[canvas] started client-to-server sync api on port', PORT)
     console.log(`[canvas] see: http://${HTTP_ADDR}:${PORT}/api`)
   })
@@ -25,4 +29,14 @@ init().then((app) => {
       console.log('[canvas] started server-to-server libp2p on port', LIBP2P_PORT)
       console.log(`[canvas] peer id: ${libp2p.peerId}`)
     })
+
+  // Unclear why node --experimental-strip-types doesn't recognize TypedEventEmitter
+  ;(app as typeof app & TypedEventEmitter<CanvasEvents & GossipLogEvents>).addEventListener("message", (event) =>{
+    const message: Message<Action> | Message<Session> = event.detail.message
+    if (message.payload.type !== "action") {
+      return
+    }
+
+    console.log('received action', message.payload)
+  })
 })

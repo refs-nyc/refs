c35253f1c4a8ce5f614e84fb4cd7dccddff319c8
Raymond Z <raykyri@gmail.com>
2025-01-24 14:05:01 -0500

    chore: avatar handling

--- ui/atoms/Avatar.tsx
+++ ui/atoms/Avatar.tsx
@@ -8,7 +8,7 @@ export const Avatar = ({ source, size = s.$3 }: { source: string; size: number }
     <>
       <View style={{ width: size, height: size }}>
         <SimplePinataImage
-          style={{ width: '100%', height: '100%', borderRadius: '100%' }}
+          style={{ width: '100%', height: '100%', borderRadius: '100%', backgroundColor: "#ddd" }}
           originalSource={source}
           imageOptions={{ width: size * 2, height: size * 2 }}
         />

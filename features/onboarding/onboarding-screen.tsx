9aa52929bb76cfdc847a1e3f9ebc689fd96c7135
manegame <contact@manusnijhoff.nl>
2025-02-27 22:30:30 +0000

    chore: update carousel lib

--- features/onboarding/onboarding-screen.tsx
+++ features/onboarding/onboarding-screen.tsx
@@ -1,10 +1,13 @@
 import { OnboardingCarouselItem } from '@/ui'
 import { View, Dimensions } from 'react-native'
+import { useSharedValue } from 'react-native-reanimated'
 import { useRef } from 'react'
 import { router } from 'expo-router'
-import Carousel, { ICarouselInstance } from 'react-native-reanimated-carousel'
+import Carousel, { ICarouselInstance, Pagination } from 'react-native-reanimated-carousel'
+import { c, s } from '@/features/style'
 
 export function OnboardingScreen() {
+  const progress = useSharedValue<number>(0)
   const ref = useRef<ICarouselInstance>(null)
   const win = Dimensions.get('window')
   const data = [{}, {}, {}]
@@ -23,11 +26,23 @@ export function OnboardingScreen() {
         loop={false}
         ref={ref}
         data={data}
+        onProgressChange={progress}
         width={win.width}
         height={win.height}
         enabled={true}
         renderItem={({ index }) => (
-          <OnboardingCarouselItem index={index} next={() => nextSlide(index)} done={done} />
+          <OnboardingCarouselItem index={index} next={() => nextSlide(index)} done={done}>
+            <Pagination.Basic
+              data={data}
+              dotStyle={{ backgroundColor: c.grey1, borderRadius: 100 }}
+              activeDotStyle={{ backgroundColor: c.grey2, borderRadius: 100 }}
+              containerStyle={{
+                gap: s.$2,
+              }}
+              progress={progress}
+              horizontal
+            />
+          </OnboardingCarouselItem>
         )}
       />
     </View>

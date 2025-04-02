9c14fb97aeb889b0d4ad7ad4ca43c80243ea4bcf
Bob Webb <bob.wbb@gmail.com>
2025-04-02 17:49:51 +0200

    remove settings route for now

--- app/user/[userName]/settings/index.tsx
+++
@@ -1,8 +0,0 @@
-import { SettingsScreen } from '@/features/user/settings-screen'
-import { Stack } from 'expo-router'
-import { useLocalSearchParams } from 'expo-router'
-
-export default function Screen() {
-  const { userName } = useLocalSearchParams()
-  return userName && <SettingsScreen userName={userName} />
-}

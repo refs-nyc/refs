a35b3ca9a0713aa47f0f25fe6d1a1013cc3ac1c2
manegame <contact@manusnijhoff.nl>
2025-04-25 11:25:33 +0200

    wip: lists

---
+++ ui/atoms/MeatballMenu.tsx
@@ -0,0 +1,29 @@
+import { View, Pressable } from 'react-native'
+import { s, c } from '@/features/style'
+
+export const MeatballMenu = ({
+  color = c.grey2,
+  onPress,
+}: {
+  color: string
+  onPress: () => void
+}) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={{
+        width: s.$2,
+        height: s.$2,
+        aspectRatio: 1,
+        gap: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'red',
+      }}
+    >
+      <View style={{ width: 5, height: 5, borderRadius: s.$05, backgroundColor: color }} />
+      <View style={{ width: 5, height: 5, borderRadius: s.$05, backgroundColor: color }} />
+      <View style={{ width: 5, height: 5, borderRadius: s.$05, backgroundColor: color }} />
+    </Pressable>
+  )
+}

3b4048d28c70ae79ccecf08b7b701c4cfd8491a9
Csilla Lukacs <erzsebet.lk@gmail.com>
2025-04-26 17:00:55 +0200

    pass profile as prop to DMButton

--- ui/profiles/DMButton.tsx
+++ ui/profiles/DMButton.tsx
@@ -1,38 +1,32 @@
-import { router, useGlobalSearchParams } from "expo-router"
+import { router } from "expo-router"
 import { Button } from "../buttons/Button"
 import { c } from "@/features/style"
 import { pocketbase, useUserStore } from "@/features/pocketbase";
 import { useEffect, useState } from "react";
-import { ConversationWithMemberships } from "@/features/pocketbase/stores/types";
-import { UsersRecord } from "@/features/pocketbase/stores/pocketbase-types";
+import { ConversationWithMemberships, Profile } from "@/features/pocketbase/stores/types";
 
-export const DMButton = () => 
+export const DMButton = ({profile} : {profile: Profile}) =>
 {
   const { user } = useUserStore();
-  if (!user) return null;
 
   const [target, setTarget] = useState<string>('');
-
-  const { userName } =  useGlobalSearchParams();
 
   useEffect(() => {
     const checkIfDirectMessageExists = async () => {
       try {
         let existingConversationId = '';
-        const userId = await pocketbase.collection<UsersRecord>('users').getFullList({
-          filter: `userName = "${userName}"`,
-        });
 
         const directConversations = await pocketbase.collection<ConversationWithMemberships>('conversations').getFullList({
           filter: `is_direct = true`,
           expand: 'memberships_via_conversation.user',
         });
         for (const conversation of directConversations) {
-          const otherUserId = conversation.expand?.memberships_via_conversation.map(m=>m.expand?.user.id).filter(id=>id!==user.id)[0];
-          if (otherUserId===userId[0].id) existingConversationId = conversation.id;
+          const otherUserId = conversation.expand?.memberships_via_conversation.map(m=>m.expand?.user.id).filter(id=>id==user?.id)[0];
+          if (otherUserId===profile.id) existingConversationId = conversation.id;
+          break;
         }
          if (!existingConversationId) {
-          setTarget(`/user/${userName}/new-dm`);
+          setTarget(`/user/${profile.userName}/new-dm`);
         }
         else
         {

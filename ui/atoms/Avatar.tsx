085f92f970a1d0f655f9c494e2b94763a85a6e10
Csilla Lukacs <erzsebet.lk@gmail.com>
2025-05-01 10:19:20 +0200

    why did prettier remove this component

--- ui/atoms/Avatar.tsx
+++ ui/atoms/Avatar.tsx
@@ -1,23 +1,73 @@
 import { SimplePinataImage } from '../images/SimplePinataImage'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import { c, s } from '@/features/style'
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons'
+import { YStack, XStack } from '../core/Stacks'
 
 export const Avatar = ({ source, size = s.$3 }: { source: string | undefined; size: number }) => {
-  if (!source) return (
-    <View style={{ width: size, height: size }}>
-      <Ionicons name="person" size={size} color={c.accent} />
-    </View>
-  )
+  if (!source)
+    return (
+      <View style={{ width: size, height: size }}>
+        <Ionicons name="person" size={size} color={c.accent} />
+      </View>
+    )
   return (
     <>
       <View style={{ width: size, height: size }}>
         <SimplePinataImage
-          style={{ width: '100%', height: '100%', borderRadius: size / 2, backgroundColor: "#ddd" }}
+          style={{ width: '100%', height: '100%', borderRadius: size / 2, backgroundColor: '#ddd' }}
           originalSource={source}
           imageOptions={{ width: size * 2, height: size * 2 }}
         />
       </View>
+    </>
+  )
+}
+
+export const AvatarStack = ({
+  sources,
+  size = s.$3,
+}: {
+  sources: (string | undefined)[]
+  size: number
+}) => {
+  let shownSources = sources.filter((s) => s) as string[]
+  if (shownSources.length > 3) shownSources = shownSources.slice(0, 3)
+
+  const others = sources.length - shownSources.length
+  const ySpacing = 5
+  const xSpacing = size / 2
+
+  return (
+    <>
+      <YStack gap={0}>
+        <XStack
+          style={{
+            height: size + (shownSources.length - 1) * ySpacing,
+            width: size + xSpacing * (shownSources.length - 1),
+            margin: 'auto',
+          }}
+        >
+          {shownSources.map((source, index) => (
+            <View
+              key={index}
+              style={{
+                position: 'relative',
+                left: -size * index + index * xSpacing,
+                top: index * ySpacing,
+              }}
+            >
+              <Avatar source={source} size={size} />
+            </View>
+          ))}
+        </XStack>
+        {others > 0 && (
+          <Text>
+            {' '}
+            and <Text style={{ fontWeight: 'bold' }}>{others} others</Text>
+          </Text>
+        )}
+      </YStack>
     </>
   )
 }

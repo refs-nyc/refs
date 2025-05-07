2fbac3c32774c637c99438d779d5004153fffca6
Csilla Lukacs <erzsebet.lk@gmail.com>
2025-05-07 14:51:03 +0200

    formatting

--- assets/icons/declarations.d.ts
+++ assets/icons/declarations.d.ts
@@ -1,5 +1,5 @@
-declare module "*.svg" {
-  import * as React from "react";
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
+declare module '*.svg' {
+  import * as React from 'react'
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+  export default ReactComponent
 }

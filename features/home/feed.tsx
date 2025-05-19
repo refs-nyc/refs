ad131e635c2996fbb5095d4bcd213ba8971bd31e
Csilla Lukacs <erzsebet.lk@gmail.com>
2025-05-19 13:41:10 +0200

    make sure logout button is visible

--- features/home/feed.tsx
+++ features/home/feed.tsx
@@ -1,16 +1,13 @@
 import type { ExpandedItem } from '@/features/pocketbase/stores/types'
 import { useState, useEffect } from 'react'
-import { DismissKeyboard, Button } from '@/ui'
+import { DismissKeyboard } from '@/ui'
 import { pocketbase } from '@/features/pocketbase'
-import { useUserStore } from '@/features/pocketbase/stores/users'
 import { View, ScrollView } from 'react-native'
-import { s } from '@/features/style'
 import { Nearby } from './nearby'
 import SearchBottomSheet from '@/ui/actions/SearchBottomSheet'
 
 export const Feed = () => {
   const [items, setItems] = useState<ExpandedItem[]>([])
-  const { logout } = useUserStore()
 
   useEffect(() => {
     const getInitialData = async () => {
@@ -38,15 +35,6 @@ export const Feed = () => {
         <ScrollView style={{ flex: 1 }}>
           <View style={{ height: '100%' }}>
             <Nearby items={items} />
-
-            <View style={{ marginBottom: s.$2, alignItems: 'center' }}>
-              <Button
-                style={{ width: 20 }}
-                variant="inlineSmallMuted"
-                title="Log out"
-                onPress={logout}
-              />
-            </View>
           </View>
         </ScrollView>
       </DismissKeyboard>

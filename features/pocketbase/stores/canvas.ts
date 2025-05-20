7fef1e5a2afb510059d1d03b53efdb4aea917f79
Bob Webb <bob.wbb@gmail.com>
2025-05-20 20:39:04 +0200

    store the metadata as json

--- features/pocketbase/stores/canvas.ts
+++ features/pocketbase/stores/canvas.ts
@@ -126,10 +126,10 @@ const refActions = {
     }
     this.db.set('ref', finalRef)
   },
-  async addRefMetadata(refId: string, { location, author }: { location: string; author: string }) {
+  async addRefMetadata(refId: string, meta: { meta: { location?: string; author?: string } }) {
     const ref = await this.db.get('ref', refId)
     if (!ref) return // TODO: ref might be missing id
-    this.db.set('ref', { ...ref, meta: { location, author } })
+    this.db.set('ref', { ...ref, meta })
   },
   async removeRef(refId: string) {
     this.db.delete('ref', refId)

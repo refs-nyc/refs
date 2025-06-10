95608471ac33388aefebb1927af985dba38f98f0
Bob Webb <bob.wbb@gmail.com>
2025-06-10 23:00:58 +0200

    fix type errors

--- ui/grid/ExampleGrid.tsx
+++ ui/grid/ExampleGrid.tsx
@@ -7,32 +7,32 @@ import { View } from 'react-native'
 export const ExampleGrid = () => (
   <View style={{ marginHorizontal: 'auto' }}>
     <GridWrapper columns={3} rows={3}>
-      <GridTileWrapper canEdit={false} type="image">
+      <GridTileWrapper type="image">
         <GridTileImage source={require('@/assets/1.png')} />
       </GridTileWrapper>
-      <GridTileWrapper canEdit={false} type="image">
+      <GridTileWrapper type="image">
         <GridTileImage source={require('@/assets/2.png')} />
       </GridTileWrapper>
-      <GridTileWrapper canEdit={false} type="image">
+      <GridTileWrapper type="image">
         <GridTileImage source={require('@/assets/3.png')} />
       </GridTileWrapper>
-      <GridTileWrapper canEdit={false} type="image">
+      <GridTileWrapper type="image">
         <GridTileImage source={require('@/assets/4.png')} />
       </GridTileWrapper>
-      <GridTileWrapper canEdit={false} type="text">
+      <GridTileWrapper type="text">
         <GridItem item={{ expand: { ref: { title: 'chinatown dumpling tier list' } } }} i={0} />
         {/* <GridTileList title="chinatown dumpling tier list" /> */}
       </GridTileWrapper>
-      <GridTileWrapper canEdit={false} type="image">
+      <GridTileWrapper type="image">
         <GridTileImage source={require('@/assets/6.png')} />
       </GridTileWrapper>
-      <GridTileWrapper canEdit={false} type="image">
+      <GridTileWrapper type="image">
         <GridTileImage source={require('@/assets/7.png')} />
       </GridTileWrapper>
-      <GridTileWrapper canEdit={false} type="image">
+      <GridTileWrapper type="image">
         <GridTileImage source={require('@/assets/8.png')} />
       </GridTileWrapper>
-      <GridTileWrapper canEdit={false} type="text">
+      <GridTileWrapper type="text">
         <GridItem item={{ expand: { ref: { title: '2025 reading list' } } }} i={0} />
       </GridTileWrapper>
     </GridWrapper>
--- ui/grid/PlaceholderGrid.tsx
+++ ui/grid/PlaceholderGrid.tsx
@@ -4,22 +4,16 @@ import { GridTile } from './GridTile'
 import { GridTileWrapper } from './GridTileWrapper'
 import { c } from '@/features/style'
 
-export const PlaceholderGrid = ({
-  columns = 3,
-  rows = 4,
-}: {
-  columns?: number
-  rows?: number
-}) => {
+export const PlaceholderGrid = ({ columns = 3, rows = 4 }: { columns?: number; rows?: number }) => {
   const gridSize = columns * rows
 
   return (
     <GridWrapper columns={columns} rows={rows}>
       {Array.from({ length: gridSize }).map((_, i) => (
-        <GridTileWrapper canEdit={false} key={`placeholder-${i}`} type="">
+        <GridTileWrapper key={`placeholder-${i}`} type="">
           <GridTile />
         </GridTileWrapper>
       ))}
     </GridWrapper>
   )
-} 
+}

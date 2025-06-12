24fe5eff5775d49e44996ee1e6957f57e9ff0ea7
Bob Webb <bob.wbb@gmail.com>
2025-06-12 13:59:04 +0200

    add backtop to remove ref sheet

--- features/pocketbase/stores/backdrop.ts
+++ features/pocketbase/stores/backdrop.ts
@@ -6,6 +6,7 @@ type BackdropState = {
   detailsBackdropAnimatedIndex?: SharedValue<number>
   otherProfileBackdropAnimatedIndex?: SharedValue<number>
   newRefSheetBackdropAnimatedIndex?: SharedValue<number>
+  removeRefSheetBackdropAnimatedIndex?: SharedValue<number>
   doPress: () => void
   setDoPress: (doPress: () => void) => void
 }
@@ -14,12 +15,14 @@ const moduleBackdropAnimatedIndex = makeMutable(0)
 const detailsBackdropAnimatedIndex = makeMutable(-1)
 const otherProfileBackdropAnimatedIndex = makeMutable(-1)
 const newRefSheetBackdropAnimatedIndex = makeMutable(-1)
+const removeRefSheetBackdropAnimatedIndex = makeMutable(-1)
 
 export const useBackdropStore = create<BackdropState>((set) => ({
   moduleBackdropAnimatedIndex,
   detailsBackdropAnimatedIndex,
   otherProfileBackdropAnimatedIndex,
   newRefSheetBackdropAnimatedIndex,
+  removeRefSheetBackdropAnimatedIndex,
   doPress: () => {},
   setDoPress: (doPress: () => void) => set({ doPress }),
 }))

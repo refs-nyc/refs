89b73e09c32aceabce5f32e30571201689ae3e53
Bob Webb <bob.wbb@gmail.com>
2025-06-17 17:23:58 +0100

    update styling for do what with X

--- ui/profiles/sheets/AddRefSheet.tsx
+++ ui/profiles/sheets/AddRefSheet.tsx
@@ -10,6 +10,7 @@ import { Text, View } from 'react-native'
 import Animated, { FadeIn, FadeOut } from 'react-native-reanimated'
 import { AddRefSheetGrid } from './AddRefSheetGrid'
 import { Heading } from '@/ui/typo/Heading'
+import { SimplePinataImage } from '@/ui/images/SimplePinataImage'
 
 export const AddRefSheet = ({
   itemToAdd,
@@ -50,11 +51,16 @@ export const AddRefSheet = ({
 
   // const [addingTo, setAddingTo] = useState<'backlog' | 'grid' | null>(null)
   const [step, setStep] = useState<
-    'chooseTarget' | 'selectItemToReplace' | 'addedToBacklog' | 'addedToGrid'
+    | 'chooseTarget'
+    | 'selectItemToReplace'
+    | 'addedToBacklog'
+    | 'addedToGrid'
+    | 'chooseReplaceItemMethod'
   >('chooseTarget')
   const [itemToReplace, setItemToReplace] = useState<ExpandedItem | null>(null)
 
-  const sheetHeight = step === 'selectItemToReplace' ? '80%' : '30%'
+  const sheetHeight =
+    step === 'selectItemToReplace' ? '80%' : step === 'chooseReplaceItemMethod' ? '50%' : '30%'
 
   const disappearsOnIndex = -1
   const appearsOnIndex = 0
@@ -108,7 +114,6 @@ export const AddRefSheet = ({
       )}
       keyboardBehavior="interactive"
     >
-      {/* firstly ask "add to backlog" or "add to grid" */}
       {step === 'chooseTarget' && (
         <View style={{ display: 'flex', flexDirection: 'column', padding: s.$3, gap: s.$1 }}>
           <Button
@@ -146,64 +151,94 @@ export const AddRefSheet = ({
           />
         </View>
       )}
-      {step === 'selectItemToReplace' &&
-        (!itemToReplace ? (
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              padding: s.$3,
-              gap: s.$1,
-              alignItems: 'center',
+      {step === 'selectItemToReplace' && (
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            padding: s.$3,
+            gap: s.$1,
+            alignItems: 'center',
+          }}
+        >
+          <Text>Choose a grid item to replace</Text>
+          <AddRefSheetGrid
+            gridItems={gridItems}
+            onSelectItem={(item) => {
+              setItemToReplace(item)
+              setStep('chooseReplaceItemMethod')
             }}
-          >
-            <Text>Choose a grid item to replace</Text>
-            <AddRefSheetGrid gridItems={gridItems} onSelectItem={setItemToReplace} />
-            <Button
-              title="Add to backlog instead"
-              variant="smallMuted"
-              onPress={async () => {
-                if (!itemToAdd) return
-                await addToProfile(itemToAdd.expand.ref, true, {
-                  backlog: true,
-                  comment: itemToAdd.text,
-                })
-                setStep('addedToBacklog')
-              }}
-            />
+          />
+          <Button
+            title="Add to backlog instead"
+            variant="smallMuted"
+            onPress={async () => {
+              if (!itemToAdd) return
+              await addToProfile(itemToAdd.expand.ref, true, {
+                backlog: true,
+                comment: itemToAdd.text,
+              })
+              setStep('addedToBacklog')
+            }}
+          />
+        </View>
+      )}
+      {step === 'chooseReplaceItemMethod' && itemToReplace && (
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+
+            padding: s.$3,
+            gap: s.$1,
+          }}
+        >
+          <View style={{ alignItems: 'center' }}>
+            <Text style={{ color: c.muted }}>Do what with {itemToReplace.expand.ref?.title}?</Text>
           </View>
-        ) : (
-          <View style={{ display: 'flex', flexDirection: 'column', padding: s.$3, gap: s.$1 }}>
-            <Text>Do what with {itemToReplace.expand.ref?.title}?</Text>
-            <Button
-              title="Remove"
-              onPress={async () => {
-                if (!itemToAdd?.expand.ref) return
-                // remove the item
-                await removeFromProfile(itemToReplace.id)
-                await addToProfile(itemToAdd.expand.ref, true, {
-                  backlog: false,
-                  comment: itemToAdd.text,
-                })
-                setStep('addedToGrid')
-              }}
-            />
-            <Button
-              title="Send to backlog"
-              onPress={async () => {
-                if (!itemToAdd?.expand.ref) return
-                // send itemToReplace to the backlog
-                await moveToBacklog(itemToReplace.id)
-                // replace the item
-                await addToProfile(itemToAdd.expand.ref, true, {
-                  backlog: false,
-                  comment: itemToAdd.text,
-                })
-                setStep('addedToGrid')
-              }}
-            />
-          </View>
-        ))}
+          {/* display the image of the item to be replaced */}
+          {itemToReplace.expand.ref?.image && (
+            <View style={{ alignItems: 'center' }}>
+              <SimplePinataImage
+                originalSource={itemToReplace.expand.ref?.image}
+                style={{ height: 100, width: 100 }}
+                imageOptions={{
+                  width: 100,
+                  height: 100,
+                }}
+              />
+            </View>
+          )}
+          <Button
+            title="Remove"
+            variant="basic"
+            onPress={async () => {
+              if (!itemToAdd?.expand.ref) return
+              // remove the item
+              await removeFromProfile(itemToReplace.id)
+              await addToProfile(itemToAdd.expand.ref, true, {
+                backlog: false,
+                comment: itemToAdd.text,
+              })
+              setStep('addedToGrid')
+            }}
+          />
+          <Button
+            title="Send to backlog"
+            onPress={async () => {
+              if (!itemToAdd?.expand.ref) return
+              // send itemToReplace to the backlog
+              await moveToBacklog(itemToReplace.id)
+              // replace the item
+              await addToProfile(itemToAdd.expand.ref, true, {
+                backlog: false,
+                comment: itemToAdd.text,
+              })
+              setStep('addedToGrid')
+            }}
+          />
+        </View>
+      )}
       {step === 'addedToBacklog' && (
         <View style={{ padding: s.$3 }}>
           <Heading tag="h1">{itemToAdd?.expand.ref.title} was added to the backlog</Heading>

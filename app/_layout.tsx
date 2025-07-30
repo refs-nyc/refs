8a1a8e1e505dec3f88b7235e38d70d4295b22a66
Bob Webb <bob.wbb@gmail.com>
2025-07-30 15:09:25 +0200

    remove base64 polyfill which didn't help

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,6 @@ import { polyfill as polyfillEncoding } from 'react-native-polyfill-globals/src/
 
 import 'event-target-polyfill'
 import '@/features/polyfill/custom-event-polyfill'
-import '@/features/polyfill/base-64-polyfill'
 import 'react-native-get-random-values'
 import 'fast-text-encoding'
 
--- features/polyfill/base-64-polyfill.js
+++
@@ -1,4 +0,0 @@
-import { decode as atob, encode as btoa } from 'base-64'
-
-global.atob = atob
-global.btoa = btoa

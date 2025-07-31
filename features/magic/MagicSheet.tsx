04c3d08089bfd934efaf86b64b1722e0cce61145
Bob Webb <bob.wbb@gmail.com>
2025-07-31 18:00:20 +0200

    add MagicSheet

--- features/magic/MagicSheet.tsx
+++ features/magic/MagicSheet.tsx
@@ -1,14 +1,11 @@
-import { c, s } from '@/features/style'
+import { c } from '@/features/style'
 
 import BottomSheet, { BottomSheetBackdrop } from '@gorhom/bottom-sheet'
 import { magic } from '@/features/magic'
 import { useAppStore } from '../stores'
 import { createRef, useEffect } from 'react'
 
-export const MagicSheet = ({}: // bottomSheetRef,
-{
-  // bottomSheetRef: React.RefObject<BottomSheet>
-}) => {
+export const MagicSheet = () => {
   const bottomSheetRef = createRef<BottomSheet>()
   const { showMagicSheet } = useAppStore()
   const disappearsOnIndex = -1
@@ -16,10 +13,8 @@ export const MagicSheet = ({}: // bottomSheetRef,
 
   useEffect(() => {
     if (showMagicSheet) {
-      console.log('opening sheet...')
       bottomSheetRef.current?.expand()
     } else {
-      console.log('closing sheet...')
       bottomSheetRef.current?.close()
     }
   }, [showMagicSheet])
@@ -32,13 +27,6 @@ export const MagicSheet = ({}: // bottomSheetRef,
       snapPoints={['90%']}
       index={-1}
       backgroundStyle={{ backgroundColor: c.olive, borderRadius: 50, paddingTop: 0 }}
-      onChange={(i: number) => {
-        console.log(`magic sheet position: ${i}`)
-        // if (i === -1) {
-        // setMagicPhoneNumber(null)
-        // setShowMagicSheet(false)
-        // }
-      }}
       backdropComponent={(p) => (
         <BottomSheetBackdrop
           {...p}

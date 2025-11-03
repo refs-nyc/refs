a30af8938dced0de377b50b1de17bb9a4dfb2ae5
Max Heald <max@maxheald.com>
2025-11-03 11:51:56 -0300

    Fix pocketbase start script paths

--- pocketbase/start.sh
+++ pocketbase/start.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
 
+cd /workspace
+
 # Start PocketBase with hooks support
 exec ./pocketbase/pocketbase serve \
-  --dir ./pocketbase/pb_data \
+  --dir /pb/pb_data \
   --hooksDir ./pocketbase/pb_data/pb_hooks \
   --http 0.0.0.0:${PORT:-8080}
 
